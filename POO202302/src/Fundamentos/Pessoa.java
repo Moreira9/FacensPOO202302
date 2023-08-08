0734eb35dc567ba2406a96aa6aebad6f88e9cfc3
Edson Martin Feitosa <edsonmfeitosa@hotmail.com>
2023-08-08 20:40:59 -0300

    Aula 01 - Classe | Objeto | troca de mensagens

---
+++ POO202302/src/Fundamentos/Pessoa.java
@@ -0,0 +1,28 @@
+package Fundamentos;
+
+/**
+ *
+ * @author edson
+ */
+public class Pessoa {
+    //atributos - características
+    private String nome;
+    public String telefone;
+    public int idade;
+    
+    //métodos - ações
+    public String perguntarONome(Pessoa pessoa){
+        //return "Qual é o seu nome?";
+        String conversa = "";
+        conversa += "Qual é o seu nome? \n";
+        conversa += pessoa.responderONome();
+        return conversa;
+    }
+    public void DarUmNome(String nome){
+        this.nome = nome;
+    }
+    public String responderONome(){
+        return "Meu nome é "+this.nome;
+    }
+    
+}
---
+++ POO202302/src/Fundamentos/PessoaTeste.java
@@ -0,0 +1,23 @@
+package Fundamentos;
+
+/**
+ *
+ * @author edson
+ */
+public class PessoaTeste {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+        //p.nome = "Edson";
+        p.DarUmNome("Edson");
+        p.idade = 28;
+        p.telefone = "(15) 323232";
+        //System.out.println(p.nome);
+        Pessoa p2 = new Pessoa();
+        //p.nome = "Guilherme";
+        p2.DarUmNome("Guilherme");
+        //System.out.println(p.perguntarONome());
+        //System.out.println(p.responderONome());
+        String teste = p.perguntarONome(p2);
+        System.out.println(teste);
+    }
+}
